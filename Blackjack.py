9c466be2200ad59c33f740e58decf88a3838c6d4
Logan Omelchuk <72324766+hamsandvich@users.noreply.github.com>
2022-09-25 12:40:47 -0600

    Bug Fixes/Code Clean up/  New function
    Fixed bugs Functions
    
    main()
    loadGamestate()
    saveGamestate()
    lowBetting()
    highBetting()
    
    Fixed Typos
    getIntInput()
    Menu()
    placeBet()
    
    removed global Vars
    deck
    stand
    blackjack
    gameload
    
    Added New Function Score

--- Blackjack.py
+++ Blackjack.py
@@ -1,32 +1,27 @@
 
 
+from glob import glob
 import random
 
 
-deck = ["D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","DJ","DQ","DK","DA","C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","CJ","CQ","CK","CA","H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","HJ","HQ","HK","HA","S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","SJ","SQ","SK","SA"]
-# possible a better way to make the deck in case of simplicity i just used the above method 
-#deck = [i + j for i in ['D', 'C', 'H', 'S'] for j in ['1', '2', '3' 'J', 'Q', 'K']]
-stand = 17
-blackJack = 21
-gameLoaded = False
 
 userMoney= 0
-
 name = ""
 
 def main():
 
-    if gameLoaded == False:
-        userMoney,name=loadGameState()
-        gameLoaded = True
+    global name
+    global userMoney
+
+    name, userMoney = loadGameState()
 
     if name == "":
-        print("Welcome To Black Jack\n")
-        Name = input("Please type in your name:")
+        print("\n\nWelcome To Black Jack\n")
+        name = input("Please type in your name:")
     else:
-        print("wWlcome Back {0}!\n Your Current balance is {1}$".format(name,str(userMoney)))
+        print("\n\nWelcome Back {0}! \nYour Current balance is {1}$\n\n".format(name,str(userMoney)))
     
-    print("1) Start Game  \n2) Quit ")
+    print("Main Menu\n---------\n\n1) Start Game  \n2) Quit \n")
     choice = getIntInput(2)
     menu(choice)
 
@@ -39,17 +34,22 @@ def loadGameState():
         file = open("save.txt", "r")
         l = file.readlines()
         file.close()
-        return l[0],l[1]
+        if l[0] == "" or l[1] == "":
+            if l[0] == "" and len(l[1]) >= 1:
+                return "No Name", int(l[1])
+            if len(l[0]) >= 1 and l[1] == "":
+                return l[1],100
+        return l[0].strip("\n"),int(l[1])
 
     except:
         file = open("save.txt", "x")
         file.close()
-        return 100 , ""
+        return "" , 100
 
 # fix the order of name / money
 def saveGameStat(name, userMoney):
-    lines = [name,userMoney]
-    file = open("save.txt", "r")
+    lines = [str(name),str(userMoney)]
+    file = open("save.txt", "w")
     file.write('\n'.join(lines))
     file.close
 
@@ -63,7 +63,7 @@ def getIntInput(number):
             continue
         
         if integer > number or integer <= 0:
-            print("Please, enter a input")
+            print("Please, enter a valid input")
         
         else:
             return integer
@@ -77,7 +77,8 @@ def getchar(str1, str2):
 def menu(case):
     match case:
         case 1:
-            print("1) Normal Bets (1,5,10,50,100)\n 2) HighRoller (1000,2500,5000,10000)")
+            print("\n\nWhat Type of bets would you want?\n---------------------------------\n")
+            print("1) Normal Bets (1,5,10,50,100)\n2) HighRoller (1000,2500,5000,10000)\n")
             choice = getIntInput(2)
             if choice == 1:
                 lowBetting()
@@ -90,103 +91,123 @@ def menu(case):
         case 2:
             saveGameStat(name, userMoney)
             quit()
-##########
-# Freddie was here
-##################
+
 def placeBet(betAmount1,betAmount2,betAmount3,betAmount4,betAmount5):
     firstBet = False
+    global userMoney
+    bet = 0
     while True:
-        bet = 0
         if firstBet == False:
-            print("How much do you want to bet \n 1) 1\n 2) 5 \n 3) 10 \n 4) 50 \n 5) 100")
+            print("\nHow much do you want to bet\n--------------------------- \n\n 1) 1\n 2) 5 \n 3) 10 \n 4) 50 \n 5) 100\n")
             choice = getIntInput(6)
             if choice == 1 and userMoney >= betAmount1:
                 bet += betAmount1
                 userMoney -= betAmount1
             elif  choice == 1 and userMoney < betAmount1:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             if choice == 2 and userMoney >= betAmount2:
                 bet += betAmount2
                 userMoney -= betAmount2
             elif  choice == 2 and userMoney < betAmount2:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             if choice == 3 and userMoney >= betAmount3:
                 bet += betAmount3
                 userMoney -= betAmount3
             elif  choice == 3 and userMoney < betAmount1:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             if choice == 4 and userMoney >= betAmount4:
                 bet += betAmount4
                 userMoney -= betAmount4
             elif  choice == 4 and userMoney < betAmount4:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             if choice == 5 and userMoney >= betAmount5:
                 bet += betAmount5
                 userMoney -= betAmount5
             elif  choice == 5 and userMoney < betAmount5:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             firstBet = True
-        print("\n Your current bet is {0}$ \n 1)Deal \n 2) Increase Bet" (bet))
+        print("\nYour current bet is {0}$\n-----------------------\n\n 1) Deal \n 2) Increase Bet\n".format(bet))
         choice = getIntInput(2)
         if choice == 1:
             return bet
 
         elif choice == 2:
-            print("How much do you want to add bet \n 1) 1$\n 2) 5$ \n 3) 10$ \n 4) 50$ \n 5) 100$")
+            print("\nHow much do you want to add to your bet?\n---------------------------------------- \n\n 1) 1$\n 2) 5$ \n 3) 10$ \n 4) 50$ \n 5) 100$\n")
             choice = getIntInput(6)
             if choice == 1 and userMoney >= betAmount1:
                 bet += betAmount1
                 userMoney -= betAmount1
             elif  choice == 1 and userMoney < betAmount1:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             if choice == 2 and userMoney >= betAmount2:
                 bet += betAmount2
                 userMoney -= betAmount2
             elif  choice == 2 and userMoney < betAmount2:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             if choice == 3 and userMoney >= betAmount3:
                 bet += betAmount3
                 userMoney -= betAmount3
             elif  choice == 3 and userMoney < betAmount1:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             if choice == 4 and userMoney >= betAmount4:
                 bet += betAmount4
                 userMoney -= betAmount4
             elif  choice == 4 and userMoney < betAmount4:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
             if choice == 5 and userMoney >= betAmount5:
                 bet += betAmount5
                 userMoney -= betAmount5
             elif  choice == 5 and userMoney < betAmount5:
-                print("you don't have enough for this bet you balance is {0}" (userMoney))
+                print("\nYou don't have enough for this bet. Your balance is {0}".format(userMoney))
   
-  
+def score(deckList, splt):
+
+    score = 0
+    for i in range(deckList):
+        if i[1] == "K":
+            score += 10
+        if i[1] == "J":
+            score += 10
+        if i[1] == "Q":
+            score += 10
+        else:
+            score += int(i[1:])
+    return score
+
+
+
 def deal(deck):
+    stand = 17
+    blackJack = 21
     playerScore = 0
     playHand = []
     dealerScore = 0
     dealerHand = []
+    split = False
     
-    playHand.append(deck[0])
-    deck.pop()
-    dealerHand.append(deck[0])
-    deck.pop()
-    playHand.append(deck[0])
-    deck.pop()
-    dealerHand.append(deck[0])
-    deck.pop()
+    playHand.append(deck.pop())
+    dealerHand.append(deck.pop())
+    playHand.append(deck.pop())
+    dealerHand.append(deck.pop())
+    playerScore = score(playHand)
+    dealerHand = score(dealerHand[1])
+    print("Dealer hand is {0}, {1}\n \n \n \n",(dealerHand[1], dealerScore))
 
-    
+    print("Player hand is {0}, {1}", (playHand.join(" "), playerScore) )
 
-    print("Dealer hand is {0}, {1}"(dealerHand[1], dealerScore))
+
 
 
 
 def game(bet):
+    deck = ["D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","DJ","DQ","DK","DA","C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","CJ","CQ","CK","CA","H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","HJ","HQ","HK","HA","S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","SJ","SQ","SK","SA"]
+    # possible a better way to make the deck in case of simplicity i just used the above method 
+    #deck = [i + j for i in ['D', 'C', 'H', 'S'] for j in ['1', '2', '3' 'J', 'Q', 'K']]
     playWon = False
     playerDoubleDown = False
+    
     suffleDeck = random.shuffle(deck)
-    deal(suffleDeck)
+    playerWon, playerDoubleDown = deal(suffleDeck)
 
     
 
@@ -199,7 +220,7 @@ def game(bet):
 
 def lowBetting():
     while True:
-        bet = placeBet(1,5,10,50,100,userMoney)
+        bet = placeBet(1,5,10,50,100)
         game(bet)
         print("Your balance is {0}$ \n")
         print("Play again Y or N")
@@ -212,7 +233,7 @@ def lowBetting():
 
 def highBetting():
     while True:
-        bet = placeBet(1,5,10,50,100,userMoney)
+        bet = placeBet(250,500,1000,5000,10000)
         game(bet)
         print("Your balance is {0}$ \n")
         print("Play again Y or N")
@@ -221,3 +242,5 @@ def highBetting():
             print("1) Start Game  \n2) Quit ")
             choice = getIntInput(2)
             menu(choice)
+
+main()
